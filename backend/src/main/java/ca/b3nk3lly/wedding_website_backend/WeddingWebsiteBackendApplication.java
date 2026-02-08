1f305b1b0285d7f9aa5722663eabb432fba83a1a
b3nk3lly <benjkelly98@gmail.com>
2026-02-08 14:58:57 -0400

    Initial commit

---
+++ backend/src/main/java/ca/b3nk3lly/wedding_website_backend/WeddingWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.b3nk3lly.wedding_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ca/b3nk3lly/wedding_website_backend/WeddingWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.b3nk3lly.wedding_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
